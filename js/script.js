5e2bd81508f2dfc81a5f331d8b879c6434f156a5
Caterina Bertotti <bertotti4997@gmail.com>
2021-03-25 17:12:33 +0100

    struttura base

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var app = new Vue( 
+    {
+        el: '#root',
+        data: {
+            img: {
+                'https://get.wallhere.com/photo/16-1826x1217-px-ocean-otter-sea-1653537.jpg',
+                'https://get.wallhere.com/photo/goat-muzzle-funny-1189300.jpg',
+                'https://get.wallhere.com/photo/nature-grass-wildlife-Canon-whiskers-hay-exploring-grassland-explore-fields-racoon-canon6d-nocturnal-fauna-mammal-babies-prairie-snout-grass-family-ecoregion-raccoon-procyonidae-procyon-viverridae-farmanimals-wildanimals-coon-babyanimals-cuteanimals-racoons-babyracoons-babyracoon-farmerscrop-terrestrial-animal-874362.jpg',
+                'https://get.wallhere.com/photo/2560x1600-px-animals-red-panda-sloths-1090870.jpg'
+            }
+        }
+    }
+
+
+)
